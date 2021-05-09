5f89ff90847d832da5f688f06a14166767c3c961
jaiswalpuru <puru.1si15cs081@gmail.com>
2021-05-09 22:09:07 +0530

    motivational bot basic feature

---
+++ motivational_bot/motivation.py
@@ -0,0 +1,28 @@
+import discord
+import os
+import requests
+import json
+
+client = discord.Client()
+
+def return_motivation():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return quote
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    message.content = message.content.replace(" ", "")
+    if message.content.startswith('$inspire'):
+        await message.channel.send(return_motivation())
+
+client.run(os.getenv("TOKEN","ODQwOTE5MzE5MzQ5NDI4MjI0.YJfNJQ.NrDgrp_TDp2uvJ0tXQ8za7MMMn4"))
+
