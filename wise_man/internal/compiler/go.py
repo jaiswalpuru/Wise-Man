831f6b989ea3f1e9fdb8d60a6deebb329b88f714
jaiswalpuru <puru.1si15cs081@gmail.com>
2021-05-24 00:13:16 +0530

    adding support for golang

--- wise_man/internal/compiler/go.py
+++ wise_man/internal/compiler/go.py
@@ -1,3 +1,21 @@
+import os
+
 def comgo(message = ""):
-    return "not implemented yet..."
+    output:str = "...hungry for code..."
+    message = str(message)
+
+    # strip the command and trailing whitespaces if any
+    code = str(message[6:]).strip()
+    if code != "":
+        # write the code to file
+        fs = open(r"junkgo.go","w+")
+        fs.write(code)
+        fs.close()
+        os.system('go fmt junkgo.go')
+        #execute shell command store the output in var and return
+        stream = os.popen('go run junkgo.go')
+        output = stream.read()
+        os.system("rm junkgo.go") # remove this line to debug the bot locally
+
+    return output
 
